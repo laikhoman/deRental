5b07cd514287904de4460d5c7839082735ef42f5
0xDevaradise <laikhoman@yahoo.com>
2024-06-08 20:59:14 +0700

    matic main rpc

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,20 +6,20 @@ module.exports = {
     localhost: {
       url: 'http://127.0.0.1:8545',
     },
-    bitfinity: {
-      url: 'https://testnet.bitfinity.network',
-      accounts: ['647cb887568119af7d43e8642bca02b7dd2bd6d0b61b522ee1d9165a60cb8f70'], // [process.env.DEPLOYER],
-      chainId: 355113,
-      timeout: 120000,
-      gasPrice: 10 * 10**9,
-    },
-    // MUMBAI_RPC: {
-    //   url: 'https://rpc-mumbai.maticvigil.com/v1/92ade6c99ebf54cfb388e446539ff00d317786be',
-    //   accounts: [process.env.DEPLOYER],
-    //   chainId: 80001,
+    // bitfinity: {
+    //   url: 'https://testnet.bitfinity.network',
+    //   accounts: ['647cb887568119af7d43e8642bca02b7dd2bd6d0b61b522ee1d9165a60cb8f70'], // [process.env.DEPLOYER],
+    //   chainId: 355113,
     //   timeout: 120000,
     //   gasPrice: 10 * 10**9,
     // },
+    matic_rpc: {
+      url: 'https://rpc-mainnet.maticvigil.com/v1/92ade6c99ebf54cfb388e446539ff00d317786be',
+      accounts: ['e6bbb4065bbfd181d4d037e25049fc8b5c5c6f091c869a4340b49425f0168e8e'],
+      chainId: 137,
+      timeout: 120000,
+      gasPrice: 10 * 10**9,
+    },
     
   },
   solidity: {
